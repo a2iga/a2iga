e23d0a4ba76f7dbf3f9554b3384dc69602c2b897
rx1310 <rx1310@inbox.ru>
2020-08-15 17:04:00 +0300

    Добавил активность, которая будет запускать ассистент-приложение

---
+++ app/src/main/java/o1310/rx1310/app/a2iga/LaunchAssistant.java
@@ -0,0 +1,15 @@
+package o1310.rx1310.app.a2iga;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LaunchAssistant extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+	
+}
