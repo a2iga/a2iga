ce86a42a8578956752179325fce3c8a75319a29a
rx1310 <rx1310@inbox.ru>
2021-02-06 19:09:29 +0300

    Начало разработки A2IGA 2

---
+++ app/src/main/java/ru/rx1310/app/a2iga/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package ru.rx1310.app.a2iga.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import ru.rx1310.app.a2iga.R;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
