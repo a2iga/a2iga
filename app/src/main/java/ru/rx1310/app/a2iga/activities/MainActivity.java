2d868c158a5a0135be204ccf8aeded1bddc59f77
rx1310 <rx1310@inbox.ru>
2021-02-08 22:05:27 +0300

    Добавил карточку для Android 5 и ниже о несовместимости приложения

--- app/src/main/java/ru/rx1310/app/a2iga/activities/MainActivity.java
+++ app/src/main/java/ru/rx1310/app/a2iga/activities/MainActivity.java
@@ -7,13 +7,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import ru.rx1310.app.a2iga.R;
 import android.content.Intent;
+import android.support.v7.widget.CardView;
+import android.os.Build;
 
 public class MainActivity extends AppCompatActivity {
     
+	CardView msgUncompatApi22;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+		
+		msgUncompatApi22 = findViewById(R.id.uncompatibilityApi22Message);
+		if (Build.VERSION.SDK_INT >= 22) {
+			msgUncompatApi22.setVisibility(View.GONE);
+		} else {
+			msgUncompatApi22.setVisibility(View.VISIBLE);
+		}
 		
     }
 	
