0dac903d3b1eaf83c703df9c5086a3b7f56c28a6
rx1310 <rx1310@inbox.ru>
2021-02-13 23:24:30 +0300

    Добавил PromtPreference из Android-приложения @devreader

---
+++ app/src/main/java/ru/rx1310/app/a2iga/ui/preference/PromtPreference.java
@@ -0,0 +1,18 @@
+// ! rx1310 <rx1310@inbox.ru> | Copyright (c) rx1310, 2021 | MIT License
+
+package ru.rx1310.app.a2iga.ui.preference;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import ru.rx1310.app.a2iga.R;
+
+public class PromtPreference extends CategoryPreference {
+
+	public PromtPreference(Context c, AttributeSet attrs) {
+		super(c, attrs);
+		setSelectable(false);
+		setLayoutResource(R.layout.ui_preference_promt);
+	}
+
+}
