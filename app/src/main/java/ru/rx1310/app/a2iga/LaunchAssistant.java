b16cdb8598f84cf1547fde0692cf334181fe9360
rx1310 <rx1310@inbox.ru>
2021-05-08 22:20:33 +0300

    Фиксы механизма запуска приложения-ассистента

--- app/src/main/java/ru/rx1310/app/a2iga/LaunchAssistant.java
+++ app/src/main/java/ru/rx1310/app/a2iga/LaunchAssistant.java
@@ -43,120 +43,127 @@ public class LaunchAssistant extends Activity {
 	
 	void runApp(String pkgName) {
 		
-		// Если в packageName есть данные, то запускаем приложение
-		Intent i = getPackageManager().getLaunchIntentForPackage(pkgName);
+		if (TextUtils.isEmpty(pkgName)) {
 
-		if (i == null) {
+			/* Если packageName пустой, то отобразим
+			 * toast-сообщение, которое уведомляет об этом */
+			AppUtils.showToast(this, getString(R.string.pkg_name_notspecified));
 
-			/* Если package name указан, но приложение
-			 * не установлено — ищем в Play Store это приложение */
-			i = new Intent(Intent.ACTION_VIEW);
-			AppUtils.showToast(this, getString(R.string.app_not_found));
-			i.setData(Uri.parse("market://details?id=" + pkgName));
+			// Также запустим окно настроек
+			//startActivity(new Intent(this, SettingsActivity.class));
+
+		} else if (isAssistAppPkgName.contains("a2iga.module.")) {
+
+			// ? Если установлен модуль
+			oIntent.setComponent(new ComponentName(isAssistAppPkgName, isAssistAppPkgName + ".LaunchModule"));
+			startActivity(oIntent);
+
+		} else {
+
+			// Если в packageName есть данные, то запускаем приложение
+			Intent i = getPackageManager().getLaunchIntentForPackage(pkgName);
+
+			if (i == null) {
+
+				/* Если package name указан, но приложение
+				 * не установлено — ищем в Play Store это приложение */
+				i = new Intent(Intent.ACTION_VIEW);
+				AppUtils.showToast(this, getString(R.string.app_not_found));
+				i.setData(Uri.parse("market://details?id=" + pkgName));
+
+			}
+
+			i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+			startActivity(i);
 
 		}
-
-		i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-		startActivity(i);
-
+		
 		this.finish();
 		
 	}
 
 	// Функция запуска ассистента
 	public void startAssistApp(String pkgName) {
-
-		if (TextUtils.isEmpty(pkgName)) {
-
-			/* Если packageName пустой, то отобразим
-			 * toast-сообщение, которое уведомляет об этом */
-			AppUtils.showToast(this, getString(R.string.pkg_name_notspecified));
-			
-			// Также запустим окно настроек
-			//startActivity(new Intent(this, SettingsActivity.class));
-
-		} else if (isAssistAppPkgName.contains("a2iga.module.")) {
-			
-			// ? Если установлен модуль
-			oIntent.setComponent(new ComponentName(isAssistAppPkgName, isAssistAppPkgName + ".LaunchModule"));
-			startActivity(oIntent);
-			
-		} else {
-
-			runApp(pkgName);
-			
-		}
+		
+		runApp(pkgName);
 
 	}
 	
 	// Функция запуска ассистента через подтверждение отпечатка пальца
 	public void startAssistAppWithFingerprint(final String pkgName) {
 		
-		final android.support.v7.app.AlertDialog.Builder b = new android.support.v7.app.AlertDialog.Builder(LaunchAssistant.this, R.style.AppTheme_Dialog_Alert);
-
-		b.setTitle(R.string.fingerprint_perm_dialog);
-		b.setMessage(String.format(getString(R.string.fingerprint_perm_dialog_desc), AppUtils.getAppName(this, isAssistAppPkgName)));
-		b.setCancelable(false);
-		
-		if (isFingerprintPermDialogAppIconEnabled) {
+		if (TextUtils.isEmpty(pkgName)) {
 			
-			try {
-				Drawable drawable = getPackageManager().getApplicationIcon(isAssistAppPkgName);
-				b.setIcon(drawable);
-			} catch (PackageManager.NameNotFoundException e) {
-				e.printStackTrace();
-			}
+			AppUtils.showToast(this, getString(R.string.pkg_name_notspecified));
 			
 		} else {
-			b.setIcon(R.drawable.ic_app_logo);
-		}
-		
-		b.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() { // обработка нажатия кнопки "No"
-			public void onClick(DialogInterface d, int i) {
-				finish();
-			}
-		});
-		
-		b.create();
-		b.show();
-		
-		if (FingerprintHelper.canUseFingerprint()) {
 			
-			try {
-				
-				oFingerprintHelper = new FingerprintHelper() {
-					
-					@Override
-					public void onAuthenticationError(int errCode, CharSequence errMessage) {
-						AppUtils.showToast(LaunchAssistant.this, errMessage + "");
-						AppUtils.Log(LaunchAssistant.this, "e", "onAuthenticationError || errorCode: " + errCode + " || errorMessage: " + errMessage);
-					}
+			final android.support.v7.app.AlertDialog.Builder b = new android.support.v7.app.AlertDialog.Builder(LaunchAssistant.this, R.style.AppTheme_Dialog_Alert);
 
-					@Override
-					public void onAuthenticationHelp(int helpCode, CharSequence helpMessage) {
-						AppUtils.showToast(LaunchAssistant.this, "Help string: " + helpMessage + " / " + helpCode);
-					}
+			b.setTitle(R.string.fingerprint_perm_dialog);
+			b.setMessage(String.format(getString(R.string.fingerprint_perm_dialog_desc), AppUtils.getAppName(this, isAssistAppPkgName)));
+			b.setCancelable(false);
 
-					@Override
-					public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
-						runApp(pkgName);
-					}
+			if (isFingerprintPermDialogAppIconEnabled) {
 
-					@Override
-					public void onAuthenticationFailed() {
-						AppUtils.showToast(LaunchAssistant.this, getString(R.string.fingerprint_perm_denied));
-						//finish();
-					}
-					
-				};
-				
-				oFingerprintHelper.startAuth();
-				
-			} catch (Exception e) {
-				e.printStackTrace();
+				try {
+					Drawable drawable = getPackageManager().getApplicationIcon(isAssistAppPkgName);
+					b.setIcon(drawable);
+				} catch (PackageManager.NameNotFoundException e) {
+					e.printStackTrace();
+				}
+
+			} else {
+				b.setIcon(R.drawable.ic_app_logo);
 			}
-			
+
+			b.setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() { // обработка нажатия кнопки "No"
+					public void onClick(DialogInterface d, int i) {
+						finish();
+					}
+				});
+
+			b.create();
+			b.show();
+
+			if (FingerprintHelper.canUseFingerprint()) {
+
+				try {
+
+					oFingerprintHelper = new FingerprintHelper() {
+
+						@Override
+						public void onAuthenticationError(int errCode, CharSequence errMessage) {
+							AppUtils.showToast(LaunchAssistant.this, errMessage + "");
+							AppUtils.Log(LaunchAssistant.this, "e", "onAuthenticationError || errorCode: " + errCode + " || errorMessage: " + errMessage);
+						}
+
+						@Override
+						public void onAuthenticationHelp(int helpCode, CharSequence helpMessage) {
+							AppUtils.showToast(LaunchAssistant.this, "Help string: " + helpMessage + " / " + helpCode);
+						}
+
+						@Override
+						public void onAuthenticationSucceeded(FingerprintManager.AuthenticationResult result) {
+							runApp(pkgName);
+						}
+
+						@Override
+						public void onAuthenticationFailed() {
+							AppUtils.showToast(LaunchAssistant.this, getString(R.string.fingerprint_perm_denied));
+							//finish();
+						}
+
+					};
+
+					oFingerprintHelper.startAuth();
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+			}
 		}
 		
 	}
