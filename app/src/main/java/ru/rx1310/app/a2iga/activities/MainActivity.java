14838ad9670d15c8a7f079ee6a92cee1eb43c571
rx1310 <rx1310@inbox.ru>
2021-05-09 16:03:16 +0300

    Empty commit

--- app/src/main/java/ru/rx1310/app/a2iga/activities/MainActivity.java
+++ app/src/main/java/ru/rx1310/app/a2iga/activities/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 	LinearLayout oCurrentAssistAppLayout, oBetaVersionInstalledMsgLayout, oModuleSettingsLayout;
 	SharedPreferences oSharedPreferences;
 	Intent oIntent;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
