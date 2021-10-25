b17b7a349e27e7c90fbbf398e7a65205c372b9c1
rx1310 <rx1310.private@gmail.com>
2021-10-25 08:43:18 +0300

    Добавляет файлы проекта

---
+++ README.md
@@ -0,0 +1,2 @@
+# :zap: A2IGA – Any App Instead Google Assistant
+Данная ветка находится на этапе прототипирования!
---
+++ app/src/main/java/ru/rx1310/app/a2iga/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.rx1310.app.a2iga.activities;
+import android.app.Activity;
+import android.os.Bundle;
+import ru.rx1310.app.a2iga.R;
+
+public class MainActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+	
+}
