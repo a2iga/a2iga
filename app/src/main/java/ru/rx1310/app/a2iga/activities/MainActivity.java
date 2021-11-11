2e0f866a5ad63e65ea05f26c9b0649deee2184a5
rx1310 <rx1310@inbox.ru>
2021-11-12 02:42:28 +0300

    Небольшие доработки проекта

--- app/src/main/java/ru/rx1310/app/a2iga/activities/MainActivity.java
+++ app/src/main/java/ru/rx1310/app/a2iga/activities/MainActivity.java
@@ -1,5 +1,18 @@
 package ru.rx1310.app.a2iga.activities;
 
-public class MainActivity
-{
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import ru.rx1310.app.a2iga.R;
+
+public class MainActivity extends AppCompatActivity {
+		
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+				
+				super.onCreate(savedInstanceState);
+				
+				setContentView(R.layout.activity_main);
+        
+		}
+    
 }
